1502921fb0f26515f47a2f469ecf938b763d3617
Martin <Martin@Martin>
2012-11-21 01:34:20 +0100

    removed unused classes

--- DeskClock/src/cz/mpelant/deskclock/ScreensaverActivity.java
+++ DeskClock/src/cz/mpelant/deskclock/ScreensaverActivity.java
@@ -54,12 +54,12 @@ import cz.mpelant.deskclock.R;
 
 import cz.mpelant.deskclock.Utils.ScreensaverMoveSaverRunnable;
 
-
 import java.util.Calendar;
 
 public class ScreensaverActivity extends Activity {
     static final boolean DEBUG = false;
     static final String TAG = "DeskClock/ScreensaverActivity";
+    public static final String KEY_CLOCK_STYLE = "clock_style";
 
     // This value must match android:defaultValue of
     // android:key="screensaver_clock_style" in dream_settings.xml
@@ -75,16 +75,12 @@ public class ScreensaverActivity extends Activity {
     private PendingIntent mQuarterlyIntent;
     private String mClockStyle;
     private boolean mPluggedIn = true;
-    private final int mFlags = (WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-            | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-            | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
-            | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    private final int mFlags = (WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            boolean changed = intent.getAction().equals(Intent.ACTION_TIME_CHANGED)
-                    || intent.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED);
+            boolean changed = intent.getAction().equals(Intent.ACTION_TIME_CHANGED) || intent.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED);
             if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
                 mPluggedIn = true;
                 setWakeLock();
@@ -105,7 +101,8 @@ public class ScreensaverActivity extends Activity {
     };
 
     public ScreensaverActivity() {
-        if (DEBUG) Log.d(TAG, "Screensaver allocated");
+        if (DEBUG)
+            Log.d(TAG, "Screensaver allocated");
         mMoveSaverRunnable = new ScreensaverMoveSaverRunnable(mHandler);
     }
 
@@ -125,12 +122,9 @@ public class ScreensaverActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        Intent chargingIntent =
-                registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        Intent chargingIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         int plugged = chargingIntent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        mPluggedIn = plugged == BatteryManager.BATTERY_PLUGGED_AC
-                || plugged == BatteryManager.BATTERY_PLUGGED_USB
-                || plugged == BatteryManager.BATTERY_PLUGGED_WIRELESS;
+        mPluggedIn = plugged == BatteryManager.BATTERY_PLUGGED_AC || plugged == BatteryManager.BATTERY_PLUGGED_USB || plugged == BatteryManager.BATTERY_PLUGGED_WIRELESS;
 
         mDateFormat = getString(R.string.abbrev_wday_month_day_no_year);
         mDateFormatForAccessibility = getString(R.string.full_wday_month_day_no_year);
@@ -140,11 +134,8 @@ public class ScreensaverActivity extends Activity {
         mHandler.post(mMoveSaverRunnable);
 
         long alarmOnQuarterHour = Utils.getAlarmOnQuarterHour();
-        mQuarterlyIntent = PendingIntent.getBroadcast(
-                this, 0, new Intent(Utils.ACTION_ON_QUARTER_HOUR), 0);
-        ((AlarmManager) getSystemService(Context.ALARM_SERVICE)).setRepeating(
-                AlarmManager.RTC, alarmOnQuarterHour, AlarmManager.INTERVAL_FIFTEEN_MINUTES,
-                mQuarterlyIntent);
+        mQuarterlyIntent = PendingIntent.getBroadcast(this, 0, new Intent(Utils.ACTION_ON_QUARTER_HOUR), 0);
+        ((AlarmManager) getSystemService(Context.ALARM_SERVICE)).setRepeating(AlarmManager.RTC, alarmOnQuarterHour, AlarmManager.INTERVAL_FIFTEEN_MINUTES, mQuarterlyIntent);
     }
 
     @Override
@@ -159,11 +150,12 @@ public class ScreensaverActivity extends Activity {
     public void onStop() {
         unregisterReceiver(mIntentReceiver);
         super.onStop();
-   }
+    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
-        if (DEBUG) Log.d(TAG, "Screensaver configuration changed");
+        if (DEBUG)
+            Log.d(TAG, "Screensaver configuration changed");
         super.onConfigurationChanged(newConfig);
         mHandler.removeCallbacks(mMoveSaverRunnable);
         layoutClockSaver();
@@ -188,11 +180,9 @@ public class ScreensaverActivity extends Activity {
     }
 
     private void setClockStyle() {
-        Utils.setClockStyle(this, mDigitalClock, mAnalogClock,
-                SettingsActivity.KEY_CLOCK_STYLE);
+        Utils.setClockStyle(this, mDigitalClock, mAnalogClock, KEY_CLOCK_STYLE);
         mSaverView = findViewById(R.id.main_clock);
-        mClockStyle = (mSaverView == mDigitalClock ?
-                Utils.CLOCK_TYPE_DIGITAL : Utils.CLOCK_TYPE_ANALOG);
+        mClockStyle = (mSaverView == mDigitalClock ? Utils.CLOCK_TYPE_DIGITAL : Utils.CLOCK_TYPE_ANALOG);
         Utils.dimClockView(true, mSaverView);
     }
 
@@ -208,11 +198,9 @@ public class ScreensaverActivity extends Activity {
 
         mMoveSaverRunnable.registerViews(mContentView, mSaverView);
 
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-        Utils.updateDate(mDateFormat, mDateFormatForAccessibility,mContentView);
+        Utils.updateDate(mDateFormat, mDateFormatForAccessibility, mContentView);
         Utils.refreshAlarm(ScreensaverActivity.this, mContentView);
     }
 
