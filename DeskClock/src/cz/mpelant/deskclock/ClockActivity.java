66df72526565667f46aeda6c2b31e9be28dd68df
Martin <Martin@Martin>
2012-11-24 01:20:04 +0100

    Clock actvity

---
+++ DeskClock/src/cz/mpelant/deskclock/ClockActivity.java
@@ -0,0 +1,15 @@
+package cz.mpelant.deskclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClockActivity extends Activity{
+    public static final String TAG = "ClockActivity";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       
+        super.onCreate(savedInstanceState);
+    }
+
+}
